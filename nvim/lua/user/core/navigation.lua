6f72b00081764accc25e70f33420562e18eb5f1f
yohans-kasaw <emailyohans@gmail.com>
2025-06-14 07:23:15 +0300

    remove hard time

--- nvim/lua/user/core/navigation.lua
+++ nvim/lua/user/core/navigation.lua
@@ -21,19 +21,6 @@ return {
 			},
 		},
 		{
-			"m4xshen/hardtime.nvim",
-			lazy = false,
-			dependencies = { "MunifTanjim/nui.nvim" },
-			opts = {
-				disabled_keys = {
-					["<Up>"] = false,
-					["<Down>"] = false,
-					["<Left>"] = false,
-					["<Right>"] = false,
-				},
-			},
-		},
-		{
 			"leath-dub/snipe.nvim",
 			opts = {
 				ui = {
