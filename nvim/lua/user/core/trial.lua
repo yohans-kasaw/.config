aec064d4468abbb6f1c6cc15e45facf106bc6702
yohans-kasaw <emailyohans@gmail.com>
2025-07-01 22:09:16 +0300

    added back vue_ls

--- nvim/lua/user/core/trial.lua
+++ nvim/lua/user/core/trial.lua
@@ -4,6 +4,15 @@ return {
 			{ "wakatime/vim-wakatime", lazy = false },
 		},
 		{
+			"lowitea/aw-watcher.nvim",
+			opts = {
+				aw_server = {
+					host = "127.0.0.1",
+					port = 5600,
+				},
+			},
+		},
+		{
 			"epwalsh/obsidian.nvim",
 			lazy = true,
 			ft = "markdown",
