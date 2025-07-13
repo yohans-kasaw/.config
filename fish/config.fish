4cec34f2a20ad9c99959c1f0109ab427335a96a6
yohans-kasaw <emailyohans@gmail.com>
2025-07-13 21:47:27 +0300

    path and alias

--- fish/config.fish
+++ fish/config.fish
@@ -12,23 +12,15 @@ if status is-interactive
     set -x OPENROUTER_API_KEY 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
 
     alias r ranger
-    alias g git
-    alias t ts-node 
-    alias n node 
     alias t touch
-    alias r "rescan & rofi-wifi-menu.sh"
-    alias rescan "nmcli device wifi rescan"
-    alias fr "python ~/projects/focus_read/main.py"
     alias v "nvim"
     alias nv "neovide & disown"
     alias l "ls -a"
     alias floey "tmuxifier load-session floey"
     alias owlbox "tmuxifier load-session owlbox"
     alias skill "tmuxifier load-session skill-sync"
-    alias tn "python /home/yohansh/.config/fish/scripts/take_note.py"
-    alias gdnv 'neovide $(git diff main --name-only) & disown'
-
-    alias vd 'DELTA_FEATURES=+side-by-side git diff'
+    alias gac "git add .;git commit -m"
+    alias gd "git diff"
 end
 
 eval (tmuxifier init - fish)
@@ -39,3 +31,6 @@ source ~/.config/fish/zoxide.fish
 starship init fish | source
 
 __auto_git_fetch
+
+# uv
+fish_add_path "/home/yohansh/.local/bin"
