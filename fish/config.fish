504a10b2d7e34afe0954eef7fed2007d05969015
yohans-kasaw <emailyohans@gmail.com>
2025-07-30 22:20:43 +0300

    fzf fd config

--- fish/config.fish
+++ fish/config.fish
@@ -3,11 +3,14 @@ if status is-interactive
     set -x VISUAL nvim
     set -x EDITOR nvim
 
-    set -x FZF_DEFAULT_COMMAND "fd --type f"
-    set -x FZF_DEFAULT_OPTS "--style full --preview 'bat --color=always {}'"
+    set -gx FZF_DEFAULT_COMMAND  "fd --exclude .git 
+                                --exclude ~/.config/fd/fzfcd-ignore-rules ."
+
+    set -x FZF_DEFAULT_OPTS "--tiebreak=index --style full --smart-case --preview 'bat --color=always {}'"
     set fzf_preview_dir_cmd eza --all --color=always
     set fzf_fd_opts --hidden --max-depth 5
     set fzf_directory_opts --bind "ctrl-o:execute($EDITOR {} &> /dev/tty)"
+
 
     alias t touch
     alias v "nvim"
@@ -15,13 +18,17 @@ if status is-interactive
     alias floey "tmuxifier load-session floey"
     alias owlbox "tmuxifier load-session owlbox"
     alias skill "tmuxifier load-session skill-sync"
-end
 
-starship init fish | source
-fzf --fish | source
-eval (tmuxifier init - fish)
+    fzf --fish | source
+
+    __auto_git_fetch
+    starship init fish | source
+    eval (tmuxifier init - fish)
+
+    bind \cf 'fzfopen'
+end
 
 fish_add_path "/home/yohansh/.local/bin"
 fish_add_path "/home/yohansh/.cargo/bin"
 
-__auto_git_fetch
+
