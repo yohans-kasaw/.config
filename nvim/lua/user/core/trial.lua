60781302d50431e7f684bbf35735c48983c5661c
yohans <emailyyohans@gmail.com>
2025-08-05 16:58:10 +0300

    remove arrow

--- nvim/lua/user/core/trial.lua
+++ nvim/lua/user/core/trial.lua
@@ -1,17 +1,6 @@
 return {
 	plugins = {
 		{
-			"otavioschwanck/arrow.nvim",
-			opts = {
-				buffer_leader_key = "m",
-				leader_key = "<leader>;",
-				mappings = {
-					next_item = "h",
-					prev_item = "n",
-				},
-			},
-		},
-		{
 			"ptdewey/yankbank-nvim",
 			dependencies = "kkharji/sqlite.lua",
 			config = function()
