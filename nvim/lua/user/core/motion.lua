a8fef43f832709397733197e26662e27ecc53b0c
yohans <emailyyohans@gmail.com>
2025-08-06 14:55:55 +0300

    some formating

--- nvim/lua/user/core/motion.lua
+++ nvim/lua/user/core/motion.lua
@@ -18,9 +18,21 @@ return {
 			"chrisgrieser/nvim-spider",
 			lazy = true,
 			keys = {
-				{ "w", "<cmd>lua require('spider').motion('w')<CR>", mode = { "n" } },
-				{ "e", "<cmd>lua require('spider').motion('e')<CR>", mode = { "n" } },
-				{ "b", "<cmd>lua require('spider').motion('b')<CR>", mode = { "n" } },
+				{
+                    "w",
+                    "<cmd>lua require('spider').motion('w')<CR>",
+                    mode = { "n" },
+                },
+				{
+                    "e",
+                    "<cmd>lua require('spider').motion('e')<CR>",
+                    mode = { "n" },
+                },
+				{
+                    "b",
+                    "<cmd>lua require('spider').motion('b')<CR>",
+                    mode = { "n" },
+                },
 			},
 			config = function()
 				require("spider").setup({
@@ -33,9 +45,13 @@ return {
 			config = function()
 				require("leap").setup({
 					safe_labels = {},
-					preview_filter = function()
-						return false
-					end,
+					preview_filter = function() return false end,
+                    on_beacons = function (targets, _, _)
+                      for _, t in ipairs(targets) do
+                        if t.label and t.beacon then t.beacon[1] = 0 end
+                      end
+                      return true
+                    end
 				})
 			end,
 		},
@@ -91,41 +107,125 @@ return {
 			harpoon.nav_file(8)
 		end)
 
-		vim.api.nvim_set_keymap("n", "<leader>bc", "<cmd>bd<CR>", { noremap = true, silent = false })
+		vim.api.nvim_set_keymap(
+            "n",
+            "<leader>bc",
+            "<cmd>bd<CR>",
+            { noremap = true, silent = false }
+        )
 		vim.keymap.set("n", "<leader>ba", function()
 			for _, buf in ipairs(vim.api.nvim_list_bufs()) do
 				if buf ~= vim.api.nvim_get_current_buf() then
 					vim.api.nvim_buf_delete(buf, { force = false })
 				end
 			end
-		end, { noremap = true, silent = true, desc = "Close Other Buffers (Tab All)" })
+		end, {
+            noremap = true,
+            silent = true,
+            desc = "Close Other Buffers (Tab All)",
+        })
 
 		vim.keymap.set("n", "<leader>rr", function()
 			require("ranger-nvim").open(true)
 		end, { noremap = true, desc = "open ranger" })
 
-		vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true, silent = true })
-		vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true, silent = true })
+		vim.api.nvim_set_keymap(
+            "n",
+            "<C-h>",
+            "<C-w>h",
+            { noremap = true, silent = true }
+        )
+		vim.api.nvim_set_keymap(
+            "n",
+            "<C-l>",
+            "<C-w>l",
+            { noremap = true, silent = true }
+        )
+		vim.api.nvim_set_keymap(
+            "n",
+            "<C-k>",
+            "<C-w>k",
+            { noremap = true, silent = true }
+        )
+		vim.api.nvim_set_keymap(
+            "n",
+            "<C-j>",
+            "<C-w>j",
+            { noremap = true, silent = true }
+        )
 
 		vim.keymap.set({ "n", "x", "o" }, "s", "<Plug>(leap)")
 		vim.keymap.set({ "n", "x", "o" }, "gs", function()
 			require("leap.remote").action()
 		end)
 
-		vim.keymap.set({ "n", "v" }, "<Down>", "<C-f>", { noremap = true, silent = false })
-		vim.keymap.set({ "n", "v" }, "<Up>", "<C-b>", { noremap = true, silent = false })
+		vim.keymap.set(
+            { "n", "v" },
+            "<Down>",
+            "<C-f>",
+            { noremap = true, silent = false }
+        )
+		vim.keymap.set(
+            { "n", "v" },
+            "<Up>",
+            "<C-b>",
+            { noremap = true, silent = false }
+        )
 
-		vim.keymap.set({ "n", "v" }, "<S-Up>", "<cmd>Treewalker Up<cr>zz", { silent = true })
-		vim.keymap.set({ "n", "v" }, "<S-Down>", "<cmd>Treewalker Down<cr>zz", { silent = true })
-		vim.keymap.set({ "n", "v" }, "<S-Left>", "<cmd>Treewalker Left<cr>zz", { silent = true })
-		vim.keymap.set({ "n", "v" }, "<S-Right>", "<cmd>Treewalker Right<cr>zz", { silent = true })
+		vim.keymap.set(
+            { "n", "v" },
+            "<S-Up>",
+            "<cmd>Treewalker Up<cr>zz",
+            { silent = true }
+        )
+		vim.keymap.set(
+            { "n", "v" },
+            "<S-Down>",
+            "<cmd>Treewalker Down<cr>zz",
+            { silent = true }
+        )
+		vim.keymap.set(
+            { "n", "v" },
+            "<S-Left>",
+            "<cmd>Treewalker Left<cr>zz",
+            { silent = true }
+        )
+		vim.keymap.set(
+            { "n", "v" },
+            "<S-Right>",
+            "<cmd>Treewalker Right<cr>zz",
+            { silent = true }
+        )
 
-		vim.keymap.set("n", "<leader>r", require("fzf-lua").resume, { desc = "resume" })
-		vim.keymap.set("n", "<leader>g", require("fzf-lua").live_grep_native, { desc = "Grep" })
-		vim.keymap.set({ "n", "x" }, "<leader>w", require("fzf-lua").grep_cword, { desc = "Search word under cursor" })
-		vim.keymap.set("n", "<leader><space>", require("fzf-lua").files, { desc = "Files" })
-		vim.keymap.set("n", "<leader>b", require("fzf-lua").buffers, { desc = "buffers" })
+		vim.keymap.set(
+            "n",
+            "<leader>r",
+            require("fzf-lua").resume,
+            { desc = "resume" }
+        )
+		vim.keymap.set(
+            "n",
+            "<leader>g",
+            require("fzf-lua").live_grep_native,
+            { desc = "Grep" }
+        )
+		vim.keymap.set(
+            { "n", "x" },
+            "<leader>w",
+            require("fzf-lua").grep_cword,
+            { desc = "Search word under cursor" }
+        )
+		vim.keymap.set(
+            "n",
+            "<leader><space>",
+            require("fzf-lua").files,
+            { desc = "Files" }
+        )
+		vim.keymap.set(
+            "n",
+            "<leader>b",
+            require("fzf-lua").buffers,
+            { desc = "buffers" }
+        )
 	end,
 }
