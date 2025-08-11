075485e1e9297cce2203228f88f4ef521ffa6ed5
yohans <emailyyohans@gmail.com>
2025-08-11 14:50:30 +0300

    options orginized

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -1,75 +1,35 @@
-vim.opt.clipboard = "unnamedplus"
-
+-- Looks
+vim.opt.termguicolors = true
 vim.opt.showtabline = 0
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
-
-vim.opt.expandtab = true
-vim.opt.swapfile = false
-vim.o.hidden = true
+vim.opt.signcolumn = "yes"
+vim.opt.number = true
+vim.opt.relativenumber = true
+vim.opt.fillchars = { eob = " " }
+vim.opt.cursorcolumn = true
+vim.opt.cursorline = true
+vim.opt.conceallevel = 2
 vim.o.showbreak = "↳⋅"
 
+-- Indentation and Formatting
+vim.opt.expandtab = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.smartindent = false
 vim.opt.autoindent = true
 vim.opt.copyindent = true
-
-vim.g.loaded_perl_provider = 0
-vim.g.loaded_ruby_provider = 0
-
-vim.opt.number = true
-vim.opt.fillchars = { eob = " " }
-
-vim.opt.conceallevel = 2
-
-vim.opt.signcolumn = "yes"
-vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
-
-vim.opt.signcolumn = "yes"
-vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
-
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-
-vim.opt.relativenumber = true
-vim.opt.scrolloff = 99999
-
-vim.opt.termguicolors = true
-vim.jumpoptions = "stack"
-
-vim.opt.confirm = true
-
-vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
-
-local function enable_spell()
-	vim.opt_local.spell = true
-	vim.opt_local.spelllang = { "en_us" }
-end
-
-vim.o.undofile = true
-vim.opt.spellcapcheck = ""
-
-vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
-	desc = "Conditional spell checking activation",
-	callback = function(args)
-		local ft = vim.bo[args.buf].filetype
-		if vim.tbl_contains({ "markdown", "text" }, ft) then
-			enable_spell()
-		end
-	end,
-})
-
-vim.g.neovide_scroll_animation_length = 0
-vim.g.neovide_cursor_animation_length = 0
-vim.g.neovide_cursor_vfx_mode = ""
-vim.g.neovide_cursor_trail_size = 0
-vim.g.neovide_confirm_quit = true
 vim.opt.linebreak = true
 vim.opt.breakindent = true
-vim.o.guifont = "Monaspace Neon Frozen:h27"
 
-vim.opt.cursorcolumn = true
-vim.opt.cursorline = true
+-- Core Editor Behavior
+vim.opt.scrolloff = 99999
+vim.o.undofile = true
+vim.opt.swapfile = false
+vim.o.hidden = true
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.jumpoptions = "stack"
 
+-- Folding
 vim.opt.foldmethod = "expr"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 vim.opt.foldtext = ""
@@ -77,4 +37,39 @@ vim.opt.foldlevelstart = 99
 vim.opt.foldlevel = 99
 vim.opt.foldnestmax = 2
 
-vim.opt.laststatus = 3
+-- Clipboard 
+vim.opt.clipboard = "unnamedplus"
+
+-- Completion
+vim.opt.completeopt = { "menu", "menuone", "noselect", "noinsert" }
+
+-- Plugins and Providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_ruby_provider = 0
+
+-- Highlights
+vim.api.nvim_set_hl(0, "LeapBackdrop", { link = "Comment" })
+
+-- spell
+vim.opt.spellcapcheck = ""
+local function enable_spell()
+  vim.opt_local.spell = true
+  vim.opt_local.spelllang = { "en_us" }
+end
+vim.api.nvim_create_autocmd({ "FileType", "BufEnter" }, {
+  desc = "Conditional spell checking activation",
+  callback = function(args)
+    local ft = vim.bo[args.buf].filetype
+    if vim.tbl_contains({ "markdown", "text" }, ft) then
+      enable_spell()
+    end
+  end,
+})
+
+-- Neovide
+vim.o.guifont = "Monaspace Neon Frozen:h27"
+vim.g.neovide_scroll_animation_length = 0
+vim.g.neovide_cursor_animation_length = 0
+vim.g.neovide_cursor_vfx_mode = ""
+vim.g.neovide_cursor_trail_size = 0
+vim.g.neovide_confirm_quit = true
