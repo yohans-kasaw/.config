59738e34954ef9ae39049d1faba8de70ec402d33
yohans-kasaw <emailyohans@gmail.com>
2025-08-15 21:48:39 +0300

    font size; harpoon removed

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -238,15 +238,6 @@ return {
 		end,
 	},
 	{
-		"ThePrimeagen/harpoon",
-		config = function()
-			require("harpoon").setup({ tabline = true })
-		end,
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-		},
-	},
-	{
 		"ibhagwan/fzf-lua",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
 		config = function()
