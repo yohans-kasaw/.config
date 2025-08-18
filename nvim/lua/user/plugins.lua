0b1a3b6ba5e83418feb9e0721c3cc48e5b7eaa70
yohans-kasaw <emailyohans@gmail.com>
2025-08-18 12:17:28 +0300

    go imports null-ls formating

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -399,7 +399,7 @@ return {
 			local null_ls = require("null-ls")
 			null_ls.setup({
 				sources = {
-					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.goimports,
 				},
 			})
 		end,
