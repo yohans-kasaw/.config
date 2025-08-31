15f636ef3f70fa9a9bb0a68f1d51edcdb7869bf4
yohans-kasaw <emailyohans@gmail.com>
2025-08-31 20:07:44 +0300

    neovim status bar file name removed

--- nvim/lua/user/functions.lua
+++ nvim/lua/user/functions.lua
@@ -1,18 +1,5 @@
 function get_grapple_status()
 	local current_buf_path = vim.api.nvim_buf_get_name(0)
-	local file_path
-	if current_buf_path == "" then
-		file_path = "[No Name]"
-	else
-		file_path = vim.fn.pathshorten(vim.fn.fnamemodify(current_buf_path, ":.:r"))
-	end
-
-	local right_part = file_path .. "%m%r%w"
-
-	if not pcall(require, "grapple") then
-		return right_part
-	end
-
 	local grapple = require("grapple")
 	local tags = grapple.tags()
 
@@ -25,5 +12,5 @@ function get_grapple_status()
 		table.insert(file_list, file_name)
 	end
 
-	return table.concat(file_list, " ") .. "%=" .. right_part
+	return table.concat(file_list, " ") .. "%="
 end
