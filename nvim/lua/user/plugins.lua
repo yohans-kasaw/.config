0514cf2d585b879ab199d05b2a596701ab8e6d34
yohans-kasaw <emailyohans@gmail.com>
2026-01-14 15:52:18 +0300

    nvim: trailing space

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -595,4 +595,13 @@ return {
 	{
 		"bullets-vim/bullets.vim",
 	},
+	{
+		"nvim-mini/mini.trailspace",
+		version = false,
+		config = function()
+			require("mini.trailspace").setup({
+				only_in_normal_buffers = true,
+			})
+		end,
+	},
 }
